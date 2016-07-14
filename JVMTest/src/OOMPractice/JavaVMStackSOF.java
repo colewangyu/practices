ab400b90fca86eabcc5434d49d054d76865a7d21
cole <colewangyu@hotmail.com>
2016-07-14 22:48:20 +0800

    1. 展现JavaVM栈OOM和SOF错误的Java代码实现； 2. 添加对JVM调试文件中的zip文件的过滤； 3. 改变Java源代码的路径，将目前的JavaVM的OOM错误的练习代码放入OOMPractice包中。

---
+++ JVMTest/src/OOMPractice/JavaVMStackSOF.java
@@ -0,0 +1,26 @@
+package OOMPractice;
+
+/**
+ * VM args: -Xss128k
+ * @author wy
+ */
+public class JavaVMStackSOF {
+	
+	private int stackLength = 1;
+	
+	public void stackLeak() {
+		stackLength++;
+		new Thread().start();
+		stackLeak();
+	}
+	
+	public static void main(String[] args) {
+		JavaVMStackSOF oom = new JavaVMStackSOF();
+		try {
+			oom.stackLeak();
+		} catch (Throwable e) {
+			System.out.println("Stack length:" + oom.stackLength);
+			throw e;
+		}
+	}
+}
