78cebaa5485e2c76eb03a444cfc6cf72ca629e43
cole <colewangyu@hotmail.com>
2016-07-17 19:23:18 +0800

    新增测试方法区OOM、运行时常量区OOM、字符串相等。

---
+++ JVMTest/src/OOMPractice/JavaMethodAreaOOM.java
@@ -0,0 +1,14 @@
+package OOMPractice;
+
+/**
+ * VM Args: -XX:PermSize=10M -XX:MaxPermSize=10M
+ * 方法去OOM试验
+ * 
+ * @author wy
+ *
+ */
+public class JavaMethodAreaOOM {
+	public static void main(String[] args) {
+		
+	}
+}
