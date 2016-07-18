72404a0844792de1757834b67db54b7a131cd5fb
cole <colewangyu@hotmail.com>
2016-07-18 11:26:43 +0800

    新增实验对象

---
+++ JVMTest/src/OOMPractice/DirectMemoryOOM.java
@@ -0,0 +1,26 @@
+package OOMPractice;
+
+import java.lang.reflect.Field;
+
+import sun.misc.Unsafe;
+
+/**
+ * VM Args: -Xmx20M -XX:MaxDirectMemorySize=10M
+ * 直接内存OOM试验
+ * 
+ * @version JDK1.6,JDK1.8
+ * @author wy
+ *
+ */
+public class DirectMemoryOOM {
+	private static final int _1MB = 1024 * 1024;
+	
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
+		Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+		unsafeField.setAccessible(true);
+		Unsafe unsafe = (Unsafe) unsafeField.get(null);
+		while(true) {
+			unsafe.allocateMemory(_1MB);
+		}
+	}
+}
--- JVMTest/src/OOMPractice/JavaMethodAreaOOM.java
+++ JVMTest/src/OOMPractice/JavaMethodAreaOOM.java
@@ -1,14 +1,35 @@
 package OOMPractice;
 
+import java.lang.reflect.Method;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
 /**
  * VM Args: -XX:PermSize=10M -XX:MaxPermSize=10M
  * 方法去OOM试验
+ * 借助CGLib使方法区出现内存溢出异常
  * 
+ * @version JDK1.6
  * @author wy
  *
  */
 public class JavaMethodAreaOOM {
 	public static void main(String[] args) {
-		
+		while(true) {
+			Enhancer enhancer = new Enhancer();
+			enhancer.setSuperclass(OOMObject.class);
+			enhancer.setUseCache(false);
+			enhancer.setCallback(new MethodInterceptor() {
+				@Override
+				public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
+					return arg3.invokeSuper(arg0, arg2);
+				}
+			});
+			enhancer.create();
+		}
 	}
+	
+	static class OOMObject{}
 }
