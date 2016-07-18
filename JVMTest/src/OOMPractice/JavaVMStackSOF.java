9d32c1ea3954cdd666b44ade1bdb98d1945ab18a
cole <colewangyu@hotmail.com>
2016-07-18 10:20:08 +0800

    修改注释

--- JVMTest/src/OOMPractice/JavaVMStackSOF.java
+++ JVMTest/src/OOMPractice/JavaVMStackSOF.java
@@ -4,6 +4,7 @@ package OOMPractice;
  * VM args: -Xss128k
  * JavaVM栈SOF试验
  * 
+ * @version JDK1.6,JDK1.8
  * @author wy
  */
 public class JavaVMStackSOF {
@@ -17,11 +18,11 @@ public class JavaVMStackSOF {
 	}
 	
 	public static void main(String[] args) throws Throwable {
-		JavaVMStackSOF oom = new JavaVMStackSOF();
+		JavaVMStackSOF sof = new JavaVMStackSOF();
 		try {
-			oom.stackLeak();
+			sof.stackLeak();
 		} catch (Throwable e) {
-			System.out.println("Stack length:" + oom.stackLength);
+			System.out.println("Stack length:" + sof.stackLength);
 			throw e;
 		}
 	}
