30107feb36cf31b537331f42f7c3a8a2df6294a0
cole <colewangyu@hotmail.com>
2016-07-21 13:56:09 +0800

    1. 删除重复项；2. 修改T.java为更贴切的名字；3. 增加对Integer和String类型的内存存储比较的注释

--- JavaStudy/src/EqualTest.java
+++ JavaStudy/src/EqualTest.java
@@ -3,35 +3,46 @@
  */
 public class EqualTest {
     public static void main(String[] args) {
-        float f = 0.4f;
-        float g = 0.4f;
-        double b = f;
-        double c = g;
-        System.out.println(c + " " + b);
 //        equalInteger();
-//        equalString();
+        equalString();
     }
 
     private static void equalInteger() {
-//        Integer a = 1000, b = 1000;
-//        Integer c = 100, d = 100;
-        Integer a = new Integer(3);
-        int c = 3;
-        Integer b = 3;
-        System.out.println(a == b);
-        System.out.println(a == b);
+        //使用new的对象
+        Integer a = 1000, b = 1000;
+        //-128到127使用缓存的对象
+        Integer c = 100, d = 100;
+        System.out.println(a == b); //false
+        System.out.println(c == d); //true
     }
 
+    /**
+     * 两个final字符串相加，不生成新的堆区对象（直接与字符串相加而不是与变量相加，字符串相当于final类型）
+     * 普通字符串相加，会生成新的堆区对象
+     */
     private static void equalString() {
+        //final关键字，编译期放入常量池。
         final String a = "Hello";
         final String b1 = "world";
+        //先把字符串放入常量池（如存在则不重复放入），返回常量池地址。
         String b2 = "world";
-        String c = "Hello world";
-        String d = a + " " + b1;
-        String e = a + " " + b2;
-        System.out.println(d == c);
-        System.out.println(e == c);
+        String c = "Helloworld";
+
+        //两个final字符串的组合，将组合字符串放入常量池（如存在则不重复放入），返回常量池地址。
+        String d = a +  b1;
+
+        //final字符串与普通常量池字符串组合，将组合字符串放入常量池（如存在则不重复放入），+号会新建立一个堆区对象，返回该对象值。
+        String e = a +  b2;
+
+        //两个final字符串组合（world相当于final），将组合字符串放入常量池（如存在则不重复放入），返回常量池地址。
+        String f = a +  "world";
+
+        System.out.println(d == c); //true
+        System.out.println(e == c); //false
+        System.out.println(e.intern() == c); //true
+        System.out.println(f == c); //true
     }
+
     final class T {
 
     }
