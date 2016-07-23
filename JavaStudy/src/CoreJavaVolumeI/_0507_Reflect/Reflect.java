97d7d13832b3be00510da4f71dbecf6ed0421290
cole <colewangyu@hotmail.com>
2016-07-23 21:45:36 +0800

    增加反射泛型和方法调用

--- JavaStudy/src/CoreJavaVolumeI/_0507_Reflect/Reflect.java
+++ JavaStudy/src/CoreJavaVolumeI/_0507_Reflect/Reflect.java
@@ -122,18 +122,74 @@ public class Reflect {
         }
     }
 
+    /**
+     * 章节：5.7.4 在运行时使用反射分析对象
+     *
+     * 测试运行时使用反射获取对象某个属性的值。
+     *
+     * 步骤：
+     *		①. 获取对象的Class对象
+     *		②. 获取对应的Field属性
+     *		③. 设置该Field属性的可访问性
+     *		④. 读取该Field属性的值
+     *
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
     public void getFieldValue() throws NoSuchFieldException, IllegalAccessException {
         Employee e = new Employee();
+        Employee e2 = new Employee();
         e.setName("Cole Wang");
+        e2.setName("Cole");
         Class clz = e.getClass();
         //获取到私有属性name
         Field f = clz.getDeclaredField("name");
         //设置私有属性的访问权限（不设置则会收到Java安全机制限制）
         f.setAccessible(true);
+        //输出对象e的name属性值
         System.out.println(f.get(e));
+        //输出对象e2的name属性值
+        System.out.println(f.get(e2));
     }
 
-    public Object CopyOfArray(Object a, int newLength) {
+    /**
+     * 章节：5.7.4 在运行时使用反射分析对象
+     *
+     * 通过反射在运行时设置对象属性
+     *
+     * 步骤：
+     * 	(1) 获取对象的Class对象
+     * 	(2) 获取对应的Field属性
+     * 	(3) 设置该Field属性的可访问性
+     * 	(4) 设置该Field属性的值（使用set(obj, val)方法）
+     *
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public void setFieldValue() throws NoSuchFieldException, IllegalAccessException {
+        Employee e = new Employee();
+        e.setName("Cole Wang");
+        //获取对象class类
+        Class clz = e.getClass();
+        //获取对象的Field
+        // 不使用clz.getField()，因为其只能获取此类和父类的属性
+        Field f = clz.getDeclaredField("name");
+        //设置Field的可访问权限
+        f.setAccessible(true);
+        //设置对象的Field的值
+        f.set(e, "cole");
+        System.out.println(e.getName());
+    }
+
+    /**
+     * 章节：5.7.5 使用反射编写泛型数组代码
+     *
+     * 复制数组，使用反射技术实现。适用于任何类型。如果不适用反射则无法适用于所有数组类型。
+     * @param a
+     * @param newLength
+     * @return
+     */
+    public Object copyOfArray(Object a, int newLength) {
 //            (1) 获取反射对象
         Class clz = a.getClass();
 //                (2) 判断是否数组对象
@@ -148,4 +204,24 @@ public class Reflect {
 //                (6) 返回新的数组
         return newArray;
     }
+
+    /**
+     * 章节：5.7.6 调用任意方法
+     *
+     * 测试通过反射调用方法。
+     *
+     */
+    public void invokeMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Employee e = new Employee();
+        //获取对象class
+        Class clz = e.getClass();
+        //获取对象的Method
+        Method m1 = clz.getDeclaredMethod("setName", String.class);
+        Method m2 = clz.getDeclaredMethod("getName");
+        //通过Method.invoke调用该方法
+        m1.invoke(e, "Cole");
+        System.out.println(m2.invoke(e));
+    }
+
+    /////end of class
 }
