fac2ca4c9df448aadf1644a1e1c93d508815fa18
cole <colewangyu@hotmail.com>
2016-09-02 00:22:17 +0800

    改进腾讯笔试题-蛇形数组的算法

--- Algorithm/src/Tencent/SnakeMatrix.java
+++ Algorithm/src/Tencent/SnakeMatrix.java
@@ -4,111 +4,69 @@ import java.util.Scanner;
 
 /**
  * Created by wy on 2016-09-01.
-<<<<<<< be832f4ee592d5ce208b317a16f68159a812ffd5
- *
-=======
  * <p/>
->>>>>>> 腾讯笔试算法题
  * 顺时针蛇形数组
  */
 public class SnakeMatrix {
-    private static int count = 0;
-    private static int row = 0, col = 0;
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         int[][] ints = new int[N][N];
-        int[] directions = new int[]{N - 1, N - 1, 0, 0};
-        int direction = 1;
-        boolean flag = false;
-        while (true) {
-            switch (direction) {
-                case 1:
-                    if (col <= directions[0]) {
-                        printToRight(ints, directions);
-                        direction = 2;
-                    } else {
-                        flag = true;
-                    }
-                    break;
-                case 2:
-                    if (row <= directions[1]) {
-                        printToBottom(ints, directions);
-                        direction = 3;
-                    } else {
-                        flag = true;
-                    }
-                    break;
-                case 3:
-                    if (col >= directions[2]) {
-                        printToLeft(ints, directions);
-                        direction = 4;
-                    } else {
-                        flag = true;
-                    }
-                    break;
-                case 4:
-                    if (row >= directions[3]) {
-                        printToTop(ints, directions);
-                        direction = 1;
-                    } else {
-                        flag = true;
-                    }
-                    break;
+        // 上下左右四个边界的下标
+        int top, bottom, left, right;
+        left = top = 0;
+        bottom = right = N - 1;
+        // 总的计数
+        int count = 1;
+        // 标记此处输出的方向，每次只能有一个为true，且必须按右下左上顺序依次循环。
+        boolean canTop, canBottom, canLeft, canRight;
+        canTop = canBottom = canLeft = false;
+        canRight = true;
+        // 方向的计数，向一个方向输出一次加一
+        int time = 0;
+        N--;
+        while (N > 0) {
+            // 每次向一个方向，依次尝试右、下、左、上
+            if (canRight) {
+                for (int i = left; i <= right; i++) {
+                    ints[top][i] = count++;
+                }
+                top++;
+                canRight = false;
+                canBottom = true;
+            } else if (canBottom) {
+                for (int i = top; i <= bottom; i++) {
+                    ints[i][right] = count++;
+                }
+                right--;
+                canBottom = false;
+                canLeft = true;
+            } else if (canLeft) {
+                for (int i = right; i >= left; i--) {
+                    ints[bottom][i] = count++;
+                }
+                bottom--;
+                canLeft = false;
+                canTop = true;
+            } else if (canTop) {
+                for (int i = bottom; i >= top; i--) {
+                    ints[i][left] = count++;
+                }
+                left++;
+                canTop = false;
+                canRight = true;
             }
-            if (flag)
-                break;
+            time++;
+            if (time % 4 == 0) {
+                N--;
+            }
         }
+        // 输出结果
         for (int[] i : ints) {
             for (int j : i) {
-                System.out.printf("%-4s", j + " ");
+                System.out.printf("%-4d", j);
             }
             System.out.println();
         }
-    }
-
-    // 向右输出
-    private static void printToRight(int[][] ints, int[] directions) {
-        for (; col <= directions[0]; col++) {
-            count++;
-            ints[row][col] = count;
-        }
-        col--;
-        row++;
-        directions[3]++;
-    }
-
-    // 向下输出
-    private static void printToBottom(int[][] ints, int[] directions) {
-        for (; row <= directions[1]; row++) {
-            count++;
-            ints[row][col] = count;
-        }
-        row--;
-        col--;
-        directions[0]--;
-    }
-
-    // 向左输出
-    private static void printToLeft(int[][] ints, int[] directions) {
-        for (; col >= directions[2]; col--) {
-            count++;
-            ints[row][col] = count;
-        }
-        col++;
-        row--;
-        directions[1]--;
-    }
-
-    // 向上输出
-    private static void printToTop(int[][] ints, int[] directions) {
-        for (; row >= directions[3]; row--) {
-            count++;
-            ints[row][col] = count;
-        }
-        row++;
-        col++;
-        directions[2]++;
     }
 }
