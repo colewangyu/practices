cd1ed1da54ba52262b6c7274d6dd774e7dbf8df3
cole <colewangyu@hotmail.com>
2016-09-04 23:06:40 +0800

    新增LeetCode的算法题

---
+++ Algorithm/test/LeetCode/EvalRPNTest.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wy on 2016-09-04.
+ */
+public class EvalRPNTest {
+
+    @Test
+    public void testEvalRPN() throws Exception {
+        assertEquals(-1, EvalRPN.evalRPN(new String[]{"3","-4","+"}));
+        assertEquals(0, EvalRPN.evalRPN(new String[]{"0","3","/"}));
+    }
+}
