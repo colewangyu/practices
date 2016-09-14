358ab41be676077f5cfd32881d7093c44b10e958
cole <colewangyu@hotmail.com>
2016-09-14 16:07:05 +0800

    1.增加对整型的测试代码 2.修改类文件的名字

--- JavaStudy/src/MyTest/EqualTest.java
+++ JavaStudy/src/MyTest/EqualTest.java
@@ -16,6 +16,31 @@ public class EqualTest {
         Integer c = 100, d = 100;
         System.out.println(a == b); //false
         System.out.println(c == d); //true
+
+        Integer i = 127;
+        Integer j = 127;
+        // -128到127在自动装箱时使用缓存，因此对象内容和地址均相同(new的地址不相同)
+        System.out.println(i == j); // true
+        i = 128;
+        j = 128;
+        System.out.println(i == j); // false
+
+        Integer i1 = 128;
+        int j1 = 128;
+        // 与基本类型比较，对象会被自动拆箱，因此会直接比较数值，而不是地址。
+        System.out.println(i1 == j1); // true
+
+        Integer i2 = new Integer(128);
+        int j2 = 128;
+        // 与基本类型比较，对象会被自动拆箱，因此会直接比较数值，而不是地址。
+        System.out.println(i2 == j2); // true
+        // 会对j2进行装箱，
+        System.out.println(i2 == new Integer(j2)); //false
+
+        Integer i3 = new Integer(1);
+        Integer j3 = new Integer(1);
+        // 两个对象==会比较地址，因为new会生成新地址，所以为false
+        System.out.println(i3 == j3); // false
     }
 
     /**
@@ -31,21 +56,17 @@ public class EqualTest {
         String c = "Helloworld";
 
         //两个final字符串的组合，将组合字符串放入常量池（如存在则不重复放入），返回常量池地址。
-        String d = a +  b1;
+        String d = a + b1;
 
         //final字符串与普通常量池字符串组合，将组合字符串放入常量池（如存在则不重复放入），+号会新建立一个堆区对象，返回该对象值。
-        String e = a +  b2;
+        String e = a + b2;
 
         //两个final字符串组合（world相当于final），将组合字符串放入常量池（如存在则不重复放入），返回常量池地址。
-        String f = a +  "world";
+        String f = a + "world";
 
         System.out.println(d == c); //true
         System.out.println(e == c); //false
         System.out.println(e.intern() == c); //true
         System.out.println(f == c); //true
-    }
-
-    final class T {
-
     }
 }
--- JavaStudy/src/MyTest/TestAccuracy.java
+++ JavaStudy/src/MyTest/TestAccuracy.java
@@ -1,15 +1,16 @@
 package MyTest;
 
-import javafx.util.converter.FormatStringConverter;
-
 /**
  * Created by cole on 2016-05-12 Thursday.
  */
-public class Test<T extends Comparable<? super T>> {
+public class TestAccuracy<T extends Comparable<? super T>> {
     public static void main(String[] args) {
+        testDouble();
+    }
+
+    static void testDouble() {
         double d1 = 0.4;
         double d2 = 1;
-
 //        d2 = d2 - 0.3 + 0.1;
         d2 = d2 - 0.99;
         d1 = d1 - 0.39;
@@ -17,8 +18,5 @@ public class Test<T extends Comparable<? super T>> {
         System.out.println(d1 == d2);
     }
 
-    public T[] merge(T[] A1, T[] A2) {
-        //TODO 代码未开始写。
-        return null;
-    }
+
 }
