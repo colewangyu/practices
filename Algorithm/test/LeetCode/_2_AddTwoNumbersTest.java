1b40c2e5f67062c519406603a5e00b6f7ac61a92
cole <colewangyu@hotmail.com>
2016-11-14 22:25:16 +0800

    LeetCode#2的代码和单元测试

---
+++ Algorithm/test/LeetCode/_2_AddTwoNumbersTest.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wy on 2016-11-14.
+ */
+public class _2_AddTwoNumbersTest {
+    _2_AddTwoNumbers t;
+
+    @Before
+    public void setUp() throws Exception {
+        t = new _2_AddTwoNumbers();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        t = null;
+    }
+
+    @Test
+    public void testAddTwoNumbers1() throws Exception {
+
+    }
+}
