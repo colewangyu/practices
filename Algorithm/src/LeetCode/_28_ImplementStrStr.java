e9e56b2a9645191f2778ccd5f974752659d18900
colewangyu <colewangyu@hotmail.com>
2016-12-04 17:31:26 +0800

    LeetCode#28代码和单元测试

---
+++ Algorithm/src/LeetCode/_28_ImplementStrStr.java
@@ -0,0 +1,42 @@
+package LeetCode;
+
+/**
+ * Created by cole on 2016-12-04.
+ * <p>
+ * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+public class _28_ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null)
+            throw new NullPointerException();
+        if (needle.length() == 0)
+            return 0;
+
+        char[] haystackChar, needleChar;
+        boolean isFind;
+        haystackChar = haystack.toCharArray();
+        needleChar = needle.toCharArray();
+        isFind = false;
+        int i;
+        for (i = -1; i < haystack.length() - needle.length(); ) {
+            if (isFind)
+                break;
+            i++;
+            int tmp = i;
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystackChar[tmp] == needleChar[j]) {
+                    tmp++;
+                    isFind = true;
+                    continue;
+                } else {
+                    isFind = false;
+                    break;
+                }
+            }
+        }
+        if (isFind)
+            return i;
+        else
+            return -1;
+    }
+}
---
+++ Algorithm/test/LeetCode/_28_ImplementStrStrTest.java
@@ -0,0 +1,60 @@
+package LeetCode;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cole on 2016-12-04.
+ */
+public class _28_ImplementStrStrTest {
+    _28_ImplementStrStr t;
+
+    @Before
+    public void setUp() throws Exception {
+        t = new _28_ImplementStrStr();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        t = null;
+    }
+
+    @Test
+    public void testStrStr1() throws Exception {
+        assertEquals(0, t.strStr("ss", "s"));
+    }
+
+    @Test
+    public void testStrStr2() throws Exception {
+        assertEquals(0, t.strStr("sa", "sa"));
+    }
+
+    @Test
+    public void testStrStr3() throws Exception {
+        assertEquals(2, t.strStr("sst", "t"));
+    }
+
+    @Test
+    public void testStrStr4() throws Exception {
+        assertEquals(0, t.strStr("", ""));
+    }
+
+    @Test
+    public void testStrStr5() throws Exception {
+        assertEquals(-1, t.strStr("ss", "sss"));
+    }
+
+    @Test
+    public void testStrStr6() throws Exception {
+        assertEquals(0, t.strStr("a", ""));
+    }
+
+    @Test
+    public void testStrStr7() throws Exception {
+        assertEquals(-1, t.strStr("asdfg", "c"));
+    }
+
+}
