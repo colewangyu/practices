15dc5cfadd1a2bc1b416a5aeffe020246c89ebcd
colewangyu <colewangyu@hotmail.com>
2017-02-14 22:09:34 +0800

    Complete leetcode #43, include code and UT.

---
+++ Algorithm/test/LeetCode/_43_MultiplyStringsTest.java
@@ -0,0 +1,42 @@
+package LeetCode;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class _43_MultiplyStringsTest {
+    _43_MultiplyStrings t;
+
+    @Before
+    public void setUp() throws Exception {
+        t = new _43_MultiplyStrings();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        t = null;
+    }
+
+    @Test
+    public void multiply01() throws Exception {
+        assertEquals("1", t.multiply("1","1"));
+    }
+
+    @Test
+    public void multiply02() throws Exception {
+        assertEquals("9801", t.multiply("99","99"));
+    }
+
+    @Test
+    public void multiply03() throws Exception {
+        assertEquals("9999", t.multiply("9999","1"));
+    }
+
+    @Test
+    public void multiply04() throws Exception {
+        assertEquals("0", t.multiply("9999","0"));
+    }
+
+}
