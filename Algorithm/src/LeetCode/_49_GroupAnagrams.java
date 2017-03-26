6a491b488deef44d7c1c0dbd811daee37c92c301
colewangyu <colewangyu@hotmail.com>
2017-03-26 16:09:46 +0800

    Complete Leetcode #49, include code and UT.

---
+++ Algorithm/src/LeetCode/_49_GroupAnagrams.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+import java.util.*;
+
+public class _49_GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map;
+        if(strs ==null || strs.length == 0)
+            return new ArrayList<List<String>>();
+        else {
+            map = new HashMap<String, List<String>>();
+            for(String s : strs) {
+                char[] chrs = s.toCharArray();
+                Arrays.sort(chrs);
+                String tmp = String.valueOf(chrs);
+                if(map.containsKey(tmp)) {
+                    map.get(tmp).add(s);
+                } else {
+                    List<String> tmpList = new ArrayList<String>();
+                    tmpList.add(s);
+                    map.put(tmp, tmpList);
+                }
+            }
+            return new ArrayList<List<String>>(map.values());
+        }
+    }
+}
