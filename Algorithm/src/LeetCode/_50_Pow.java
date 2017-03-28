f623bc4b3f07e84ec48bc0f988410a0d7cd15a49
colewangyu <colewangyu@hotmail.com>
2017-03-28 22:48:32 +0800

    Leetcode #50 AC, include code and UT.

---
+++ Algorithm/src/LeetCode/_50_Pow.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+/**
+ * Implement pow(x, n).
+ * 时间复杂度为log(n)
+ */
+public class _50_Pow {
+    public double myPow(double x, int n) {
+        double result;
+        result = 1;
+        if (n < 0) {
+            while (n < -1) {
+                if (-n % 2 == 1) {
+                    result *= x;
+                    n++;
+                } else {
+                    x *= x;
+                    n >>= 1;
+                }
+            }
+            result = 1 / (result * x);
+        } else if (n > 0) {
+            while (n > 1) {
+                if (n % 2 == 1) {
+                    result *= x;
+                    n--;
+                } else {
+                    x *= x;
+                    n >>= 1;
+                }
+            }
+            result *= x;
+        }
+        return result;
+    }
+}
